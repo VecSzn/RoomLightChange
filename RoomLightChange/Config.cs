1b32c55e19bf28bbdc546469ef973de802fe05e4
VecSzn <82099364+VecSzn@users.noreply.github.com>
2023-05-11 10:33:38 +0800

    Version 1.0.0

---
+++ RoomLightChange/Config.cs
@@ -0,0 +1,42 @@
+﻿#region Assembly XPlugin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// C:\Users\VecSzn\Desktop\Main Files\SCP服务器配置\插件\XPlugin.dll
+// Decompiled with ICSharpCode.Decompiler 7.1.0.6543
+#endregion
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using MapGeneration;
+using PlayerRoles;
+using PluginAPI.Core.Zones;
+using UnityEngine;
+
+namespace RoomLightChange
+{
+    public class Config
+    {
+        [Description("Debug")]
+        public bool Debug { get; set; } = true;
+
+        [Description("是否开启变色插件")]
+        public bool Enable { get; set; } = true;
+
+
+        [Description("可变色房间")]
+        public List<RoomName> Rooms { get; set; } = new List<RoomName>
+        {
+            RoomName.Lcz914,
+        };
+
+        [Description("可变换颜色")]
+        public List<Color> Colors { get; set; } = new List<Color>
+        {
+            Color.red,
+            Color.yellow,
+            Color.green,
+            Color.blue,
+            Color.cyan,
+            Color.white,
+            Color.magenta
+        };
+    }
+}
---
+++ RoomLightChange/EntryPoint.cs
@@ -0,0 +1,82 @@
+﻿#region Assembly XPlugin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// C:\Users\VecSzn\Desktop\Main Files\SCP服务器配置\插件\XPlugin.dll
+// Decompiled with ICSharpCode.Decompiler 7.1.0.6543
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using CommandSystem;
+using CustomPlayerEffects;
+using HarmonyLib;
+using Interactables.Interobjects.DoorUtils;
+using InventorySystem;
+using MapGeneration;
+using MapGeneration.Distributors;
+using MEC;
+using PlayerRoles;
+using PlayerStatsSystem;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Core.Items;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using UnityEngine;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace RoomLightChange
+{
+    public class Plugin
+    {
+        public static PluginHandler _pluginHandler;
+
+        public CoroutineHandle AutoNukeHandle;
+
+
+        [PluginConfig]
+        public Config Config;
+
+        private CoroutineHandle colorhanle;
+
+        public List<Scp079Generator> _generators = new List<Scp079Generator>();
+
+        public static Plugin Instance { get; private set; }
+
+
+
+        [PluginEntryPoint("RoomLightChange", "RoomLightChange", "1.0.0", "Vec")]
+        private void LoadPlugin()
+        {
+            Instance = this;
+            EventManager.RegisterEvents(this);
+        }
+
+        [PluginEvent(ServerEventType.RoundStart)]
+        private void OnRoundStart()
+        {
+            if (colorhanle.IsRunning)
+            {
+                Timing.KillCoroutines(colorhanle);
+            }
+
+            colorhanle = Timing.RunCoroutine(RandomEvent());
+        }
+        public  IEnumerator<float> RandomEvent()
+        {
+            while (Round.IsRoundStarted)
+            {
+                yield return Timing.WaitForSeconds(2);
+                FlickerableLightController[] array2 = UnityEngine.Object.FindObjectsOfType<FlickerableLightController>();
+                foreach (FlickerableLightController flc in array2)
+                {
+                    if (Config.Rooms.Contains(flc.Room.Name))
+                    {
+                        flc.WarheadLightColor = Config.Colors[new System.Random(Environment.TickCount).Next(0, 14)];
+                    };
+                }
+            }
+        }
+    }
+}
